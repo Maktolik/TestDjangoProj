21e55379d2d5de3f4785fb4c706d87d002fee715
Maktolik <maktolik97@gmail.com>
2023-08-29 00:37:05 +0300

    login/registration messages/errors + logout

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from users.views import login, registration, profile
+from users.views import login, registration, profile, logout
 
 app_name = 'users'
 
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('registration/', registration, name='registration'),
     path('profile/', profile, name='profile'),
 
--- store/users/views.py
+++ store/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -29,6 +29,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Успешная регистрация!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
@@ -52,4 +53,8 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
