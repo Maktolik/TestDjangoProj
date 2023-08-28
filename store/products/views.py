a62c72d4c4106a9e865fbf2285c02c9404d63721
Maktolik <maktolik97@gmail.com>
2023-08-28 21:29:11 +0300

    Django test proj

---
+++ store/products/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from products.models import Product, ProductCategory
+
+
+def index(request):
+    context = {
+        'title': 'Store',
+        'is_promotion': True,
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
+
+
